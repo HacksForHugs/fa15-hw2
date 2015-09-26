88dd77146ae3fa24945a8b370ee933595874d898
Helen Che <helenc@berkeley.edu>
2015-09-25 23:30:20 -0700

    finished hw3

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,3 +1,10 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
+  def initialize(str)
+  	@string = str
+  	end
+  def bar(first, optional={})
+  	
+  	return "#{first}#{@string}#{optional[:sat]}"
+  	end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,23 @@
 class PagesController < ApplicationController
   def home
-    # foo = Foobar.new "baz"
-    # @baz = foo.bar :cat, sat: :dat, dat: :sat
+    foo = Foobar.new "baz"
+    @baz = foo.bar :cat, sat: :dat, dat: :sat
   end
 
   def stringify
+    @name = params[:name]
+    @adj = params[:adjective]
     @text = "You are nothing!"
+    
+      
   end
 
-  def age
-  end
+  # def age
+  # end
 
   def person
+    @name = params[:name]
+    @age = params[:age]
+    @dude = Person.new(@name, @age) 
   end
 end
